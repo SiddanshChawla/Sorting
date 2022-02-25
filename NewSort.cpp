5bf1189a19d7332503b3b2e042437d7e03bd5b03
SiddanshChawla <chawlasiddansh@gmail.com>
2022-02-25 10:52:05 +0530

    New Sort

---
+++ NewSort.cpp
@@ -0,0 +1,15 @@
+/*
+ * NewSort.cpp
+ *
+ *  Created on: 25-Feb-2022
+ *      Author: siddanshchawla
+ */
+
+#include "NewSort.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Hello";
+	return 0;
+}
---
+++ NewSort.h
@@ -0,0 +1,17 @@
+/*
+ * NewSort.h
+ *
+ *  Created on: 25-Feb-2022
+ *      Author: siddanshchawla
+ */
+
+#ifndef NEWSORT_H_
+#define NEWSORT_H_
+
+class NewSort {
+public:
+	NewSort();
+	virtual ~NewSort();
+};
+
+#endif /* NEWSORT_H_ */
